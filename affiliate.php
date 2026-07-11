c91f46505b349e0ce79ddf2d107a7a536db4b600
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-07-11 13:14:10 +0700

    feat: white-label-zone update

--- affiliate.php
+++ affiliate.php
@@ -325,7 +325,7 @@ function get_all_users_table() {
         }
     </style>
     <div class="white-label-zone no-print">
-        <span style="padding: 60px 10px 60px 40px;float: left;font-size: 60px;">🤝</span>
+        <span style="padding: 40px 10px 40px 40px;float: left;font-size: 60px;">🤝</span>
         <div style="padding: 20px 0;">
             <h1>WooCommerce Affiliate System</h1>
             <p>ระบบพันธมิตรสำหรับ WooCommerce
