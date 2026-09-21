ba839e6ff9d15a6ab1faab03ad4cdc74c3f7d7dd
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-21 16:32:18 +0700

    feat: policy page update and added affiliate_instructions option

--- pages/member/inc/policy.php
+++ pages/member/inc/policy.php
@@ -4,10 +4,15 @@ if (!defined('ABSPATH')) {
 }
 ?>
 <div class="row">
-    <div class="card card-custom p-4 col-6" id="policy">
-        <?=get_option("affiliate_policy")?>
+    <div class="col-lg-7">
+        <div class="card card-custom p-4 col-lg-6" id="policy">
+            <?=get_option("affiliate_policy")?>
+        </div>
+        <div class="card card-custom p-4 col-lg-6" id="requirements">
+            <?=get_option("affiliate_requirements_and_conditions")?>
+        </div>
     </div>
-    <div class="card card-custom p-4 col-6" id="requirements">
-        <?=get_option("affiliate_requirements_and_conditions")?>
+    <div class="col-lg-5">
+        <?=get_option("affiliate_instructions")?>
     </div>
 </div>
--- pages/pages_content.php
+++ pages/pages_content.php
@@ -44,6 +44,14 @@ if ( ! defined( 'ABSPATH' ) ) {
             'media_buttons' => true,                   // Show "Add Media" buttons
         ));
         ?>
+        <h3 for="affiliate_instructions"><strong>คำแนะนำสำหรับพันธมิตร:</strong></h3>
+        <?php
+        wp_editor( get_option('affiliate_instructions', ''), 'affiliate_instructions', array(
+            'textarea_name' => 'affiliate_instructions', // The 'name' attribute for the form submission
+            'textarea_rows' => 15,                      // Number of visible rows
+            'media_buttons' => true,                   // Show "Add Media" buttons
+        ));
+        ?>
         <?php submit_button('บันทึกการเปลี่ยนแปลง'); ?>
     </form>
 </div>
