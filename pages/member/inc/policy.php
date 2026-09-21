785e697ea320ef969f470114a71817a2baaa15be
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-21 16:33:31 +0700

    feat: policy page update

--- pages/member/inc/policy.php
+++ pages/member/inc/policy.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
 }
 ?>
 <div class="row">
-    <div class="col-lg-7">
+    <div class="col-lg-8 row">
         <div class="card card-custom p-4 col-lg-6" id="policy">
             <?=get_option("affiliate_policy")?>
         </div>
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
             <?=get_option("affiliate_requirements_and_conditions")?>
         </div>
     </div>
-    <div class="col-lg-5">
+    <div class="col-lg-4">
         <?=get_option("affiliate_instructions")?>
     </div>
 </div>
