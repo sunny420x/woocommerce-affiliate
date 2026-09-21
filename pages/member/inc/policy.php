cf74ddc5f6a3fb27e09ef2e8ef87f349fa512e81
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-21 16:37:40 +0700

    feat: policy page update

--- pages/member/inc/policy.php
+++ pages/member/inc/policy.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 ?>
-<div class="d-flex flex-column gap-2 card card-custom p-4">
+<div class="d-flex gap-2 card card-custom p-4">
     <div id="policy">
         <?=get_option("affiliate_policy")?>
     </div>
