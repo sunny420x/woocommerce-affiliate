00995fa89e706b6944a0fe1d7c0d607b889f271d
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-22 09:14:36 +0700

    feat: policy page styling and positioning update

--- pages/member/inc/policy.php
+++ pages/member/inc/policy.php
@@ -3,6 +3,11 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 ?>
+<div class="card card-custom p-4 mb-4">
+    <div class="row" style="max-width: 800px;">
+        <?=get_option("affiliate_instructions")?>
+    </div>
+</div>
 <div class="card card-custom mb-4 p-4">
     <div class="row">
         <div class="col-lg-6" id="policy">
@@ -11,10 +16,5 @@ if (!defined('ABSPATH')) {
         <div class="col-lg-6" id="requirements">
             <?=get_option("affiliate_requirements_and_conditions")?>
         </div>
-    </div>
-</div>
-<div class="card card-custom mb-4">
-    <div class="row">
-        <?=get_option("affiliate_instructions")?>
     </div>
 </div>
