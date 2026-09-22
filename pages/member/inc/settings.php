17eb55c64a9ea33e2a3471e3e2ed0dfb82c614a8
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-22 11:12:47 +0700

    feat: settings page update

--- pages/member/inc/settings.php
+++ pages/member/inc/settings.php
@@ -33,8 +33,7 @@ if (!defined('ABSPATH')) {
             </button>
         </div>
     </form>
-    <br>
-    <h5 class="fw-bold mb-3"><i class="fa-solid fa-building-columns text-primary me-2"></i>ตั้งค่าข้อมูลส่วนตัว</h5>
+    <h5 class="fw-bold my-3"><i class="fa-solid fa-building-columns text-primary me-2"></i>ตั้งค่าข้อมูลส่วนตัว</h5>
     <div class="table-responsive">
         <table class="table">
             <tbody>
