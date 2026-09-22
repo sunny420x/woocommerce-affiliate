2d517d02f854e1c8ee1116bca22fa183006be6c1
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-22 09:39:46 +0700

    feat: global dashboard styling update

--- pages/member/dashboard.php
+++ pages/member/dashboard.php
@@ -527,10 +527,9 @@ $is_affiliate_enabled = (esc_attr(get_option('affiliate_enable', 'yes')) === 'ye
         ?>
 
         <!-- Main Content Area -->
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
-            
+        <main class="col-md-9 p-0">
             <!-- Top Bar -->
-            <div class="d-none d-sm-flex justify-content-between align-items-center mb-4">
+            <div class="d-sm-flex d-md-none justify-content-between align-items-center mb-4">
                 <button class="btn btn-dark d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa-solid fa-bars"></i>
                 </button>
