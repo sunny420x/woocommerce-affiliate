936a954f91fcc451908a3ad16da2049d05411959
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-22 09:16:48 +0700

    feat: policy page styling and positioning update

--- pages/member/inc/policy.php
+++ pages/member/inc/policy.php
@@ -4,17 +4,17 @@ if (!defined('ABSPATH')) {
 }
 ?>
 <div class="card card-custom p-4 mb-4">
-    <div class="row" style="max-width: 800px;">
-        <?=get_option("affiliate_instructions")?>
-    </div>
-</div>
-<div class="card card-custom mb-4 p-4">
     <div class="row">
-        <div class="col-lg-6" id="policy">
-            <?=get_option("affiliate_policy")?>
+        <div class="row col-lg-5" style="max-width: 1000px; margin: auto;">
+            <?=get_option("affiliate_instructions")?>
         </div>
-        <div class="col-lg-6" id="requirements">
-            <?=get_option("affiliate_requirements_and_conditions")?>
+        <div class="row col-lg-7">
+            <div class="col-lg-6" id="policy">
+                <?=get_option("affiliate_policy")?>
+            </div>
+            <div class="col-lg-6" id="requirements">
+                <?=get_option("affiliate_requirements_and_conditions")?>
+            </div>
         </div>
     </div>
 </div>
