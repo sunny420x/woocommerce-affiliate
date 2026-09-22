83b891340c98c01a580864b13a007544ad0f2cc1
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-09-22 10:08:44 +0700

    feat: member dashboard styling update

--- pages/member/inc/nav.php
+++ pages/member/inc/nav.php
@@ -68,10 +68,6 @@ if (!defined('ABSPATH')) {
                     <i class="fa-solid fa-file-contract me-2"></i>นโยบายและเงื่อนไข
                 </a>
 
-                <a class="nav-link <?= $affiliate_tab === 'requirements' ? 'active' : ''; ?>" href="/affiliate/requirements">
-                    <i class="fa-solid fa-circle-exclamation me-2"></i> คุณสมบัติและเงื่อนไข
-                </a>
-
                 <a class="nav-link <?= $affiliate_tab === 'help' ? 'active' : ''; ?>" href="/affiliate/help">
                     <i class="fa-regular fa-circle-question me-2"></i>ช่วยเหลือ
                 </a>
